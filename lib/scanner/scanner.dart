1262d82b67bc7b03d044a274c3cbef4bec346244
Prashu <1707prash@gmail.com>
2021-03-24 11:42:02 +0530

    Scanner 1

---
+++ lib/scanner/scanner.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Scanner extends StatefulWidget {
+  @override
+  _ScannerState createState() => _ScannerState();
+}
+
+class _ScannerState extends State<Scanner> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        
+      ),
+    );
+  }
+}
